1ab30ec9dc254a0efd960b9ede5da2345e642cd2
kashi <leonc4667@gmail.com>
2022-04-28 22:02:17 -0400

    users

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,30 @@
-
 const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+
+
+//get username and room from url
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
+console.log(username, room)
 
 
 const socket = io()
+
+//join chat room
+socket.emit('joinRoom', {username, room})
+
+
+
 
 //message from server
 socket.on('message', message => {
     console.log(message)
     outputMessage(message)
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight
 })
 
 //message submit 
@@ -20,6 +37,11 @@ chatForm.addEventListener('submit', e =>{
 
     //emit message to server
     socket.emit('chatMessage', msg)
+
+    //clear input
+
+    e.target.elements.msg.value = ''
+    e.target.elements.msg.focus()
 })
 
 //output message to DOM
@@ -27,9 +49,9 @@ chatForm.addEventListener('submit', e =>{
 function outputMessage(message) {
     const div = document.createElement('div')
     div.classList.add('message')
-    div.innerHTML = `	<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username}<span>${message.time}</span></p>
     <p class="text">
-       ${message}
+       ${message.text}
     </p>`
     document.querySelector('.chat-messages').appendChild(div)
 }
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require('express')  
 const socketio = require('socket.io')
-
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -11,25 +12,33 @@ const io = socketio(server)
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+const botName = 'ChatBot'
+
 //run when client connects
 
 io.on('connection', socket => {
-  
-    socket.emit('message', 'Welcome to chat!')
+    socket.on('joinRoom', ({username, room})=>{
+ //welcome current user
+    socket.emit('message', formatMessage (botName, 'Welcome to chat!'))
 
-    // broadcast when a user connects 
+ // broadcast when a user connects 
 
-    socket.broadcast.emit('message', 'A user has joined the chat')
+    socket.broadcast.emit('message', formatMessage (botName,'A user has joined the chat'))
+   
+})
 
-    //runs when a client disconnects
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
-    })
-
+   
     //listen for chatMessage
     socket.on('chatMessage', (msg) =>{
-        io.emit('message', msg)
+        io.emit('message', formatMessage ('USER', msg))
     })
+
+        //runs when a client disconnects
+        socket.on('disconnect', () => {
+            io.emit('message', formatMessage (botName,'A user has left the chat'))
+        })
+    
 })
 
 
